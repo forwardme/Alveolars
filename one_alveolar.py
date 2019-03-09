5b97947c723e2edb6c028a2d2839e1a084e7c05b
GaoYuan <forwardme@126.com>
2019-03-09 23:19:47 +0800

    add commits to one_alveolar.py

--- one_alveolar.py
+++ one_alveolar.py
@@ -1,4 +1,7 @@
 class one_alveolar:
+  #impedance: 顺应性
+  #voltop:volum relations to pressure
+  #timestep: time increases each time
   def __init__ (self, impedance, VoltoP, timestep, restime, intime, inpressure, initialvolume = 1):
     self.initialvolume = initialvolume
     self.volume = self.initialvolume
